551920a4df3ed3e8bdd494aa07c5941d9a102313
jinningzhang6 <jinningzhang6@gmail.com>
2020-07-13 13:25:31 -0700

    new layout with fragment, tab view

--- app/src/main/java/com/example/searchlol/MainActivity.java
+++ app/src/main/java/com/example/searchlol/MainActivity.java
@@ -4,13 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.preference.PreferenceManager;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,7 +25,6 @@ import com.example.searchlol.adapter.SummonerSearchAdapter;
 import com.example.searchlol.utils.HistoryUtils;
 import com.example.searchlol.utils.RiotSummonerUtils;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -102,6 +101,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.id.nav_home:
+                Intent homeIntent = new Intent(this, homeActivity.class);
+                startActivity(homeIntent);
+                return true;
             case R.id.nav_search:
                 Intent mainIntent = new Intent(this, MainActivity.class);
                 startActivity(mainIntent);
